1a03deba0402f57483530ad23f26ae30d734a30b
Anders Larsson <anders.larsson@icm.uu.se>
2019-03-21 11:16:33 +0100

    add forgotten file

---
+++ dev/import_screen.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/opt/omero/server/OMERO.server/bin/omero import --server localhost --port 4064 --user root --password devpass /share/mikro/IMX/MDCPolinaGeorgiev/exp-WIDE/ACHN-20X-P009041/2019-02-13/32/ACHN-20X-P009041_B02_s1_w1836B5662-3526-47FE-A453-0D1775E08D17.tif
---
+++ dev/import_screen_bulk.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/opt/omero/server/OMERO.server/bin/omero import --bulk /scripts/P009041_bulk.yml --server localhost --port 4064 --user root --password devpass
